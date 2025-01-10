4b70b077e9aefedef6c719941ccb3cf91dbdbc46
Blastermore <afonsocunha.dev@gmail.com>
2025-01-10 04:51:53 +0000

    DemonScene work - Scripts for demon juice, Demon fbx, lights, assets positions

---
+++ Assets/Prefabs/DemonTest/DemonIdleJuice.cs
@@ -0,0 +1,53 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public bool onlyHorizontal;
+    public float floatHeight = 0.5f; // Height of the float movement
+    public float floatSpeed = 1f; // Speed of the float movement
+     private Vector3 startPosition;
+
+    void Start()
+    {
+        Vector3 originalPosition = transform.position;
+        startPosition = transform.position;
+    }
+    void Update()
+    {
+        LookAtPlayer();
+        Floating();
+    }
+    private void LookAtPlayer()
+    {
+        if (player != null)
+        {
+            // Calculate the direction to the player
+            Vector3 directionToPlayer = player.position - transform.position;
+            if(onlyHorizontal)
+            {
+                directionToPlayer.y = 0; 
+            }
+
+            // Calculate the target rotation (only around the Y-axis)
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+            Vector3 targetEulerAngles = targetRotation.eulerAngles;
+            //targetEulerAngles.x = -90f;
+
+            targetRotation = Quaternion.Euler(targetEulerAngles);
+
+            // Smoothly rotate the object around its local axis
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+        }
+    }
+
+    private void Floating()
+    {
+        float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+
+        // Apply the floating effect to the object's position
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+}
