1ebae5f30850f58d177dd81882954a6dc11a739a
notCroptu <martinsmariana1972@gmail.com>
2025-01-11 19:22:55 +0000

    Add security camera effect to pause menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _pause;
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _holdingCamera;
+    [SerializeField] private SecurityCameraSwitcher _cameraSwitcher;
     private PlayerBehaviorControl _pbc;
+    private int _lastCameraIndex = -1;
     private void Awake()
     {
         _pbc = _player.GetComponent<PlayerBehaviorControl>();
@@ -26,17 +29,39 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         Cursor.lockState = CursorLockMode.None;
-        Time.timeScale = 0;
+        // Time.timeScale = 0;
         CheckIfInteractiveHolding();
         _pbc.EnableDisablePlayer(false);
+
+
+        // check what was teh last camera
+        _lastCameraIndex = _cameraSwitcher.IsCurrentCameraPlayer();
+        SwitchToRandomCam();
+
+
         _pause.SetActive(true);
     }
     public void Continue()
     {
+        SwitchToLastCam();
+
         _pause.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
-        Time.timeScale = 1;
+        // Time.timeScale = 1;
         CheckIfInteractiveHolding();
         _pbc.EnableDisablePlayer(true);
+    }
+
+    private void SwitchToRandomCam()
+    {
+        int ran = Random.Range(0, _cameraSwitcher.SecurityCameraAmount);
+        _cameraSwitcher.SwitchSecurityCamera(ran);
+    }
+    private void SwitchToLastCam()
+    {
+        if (_lastCameraIndex == -1)
+            _cameraSwitcher.SwitchToPlayerCamera();
+        else
+            _cameraSwitcher.SwitchSecurityCamera(_lastCameraIndex);
     }
 }
