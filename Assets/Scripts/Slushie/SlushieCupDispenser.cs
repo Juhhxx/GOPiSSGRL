87aa98f96aee8d415a60861d06431f65b09e5140
Julia Costa <juliacosta232005@gmail.com>
2025-01-13 08:52:02 +0000

    Add More Sounds, Fix Issues in Cup Dispenser Sound

--- Assets/Scripts/Slushie/SlushieCupDispenser.cs
+++ Assets/Scripts/Slushie/SlushieCupDispenser.cs
@@ -5,13 +5,7 @@ public class SlushieCupDispenser : MonoBehaviour
     [SerializeField] private PlayerInventory _playerInventory;
     [SerializeField] private SlushieCup _slushieCup;
     [SerializeField] private AudioClip _pickSound;
-    private AudioSource _audioSource;
-
-    private void Start()
-    {
-        _audioSource = gameObject.AddComponent<AudioSource>();
-        _audioSource.volume = 0.01f;
-    }
+    
     public void GiveSlushie()
     {
         _slushieCup.GiveSlushie(_playerInventory);
