318ed54b00baa573b5440851adac40878bc7445f
notCroptu <martinsmariana1972@gmail.com>
2025-02-03 22:40:27 +0000

    bigger radio range

--- Assets/Scripts/ChalkRadio/Radio.cs
+++ Assets/Scripts/ChalkRadio/Radio.cs
@@ -104,8 +104,11 @@ public class Radio : MonoBehaviour
     }  
     private void ChangeAudioVolumeDistance (float distance)
     {
-        // _mainAudio.volume = Mathf.InverseLerp(20f,0.5f,distance);
-        _mainAudio.volume = Mathf.Pow( 1.2f, -distance) + 0.05f;
+        float volume = Mathf.Pow(1.15f, -distance) + 0.05f;
+
+        volume = Mathf.Lerp(0f, 1.8f, Mathf.Clamp01(volume));
+
+        _mainAudio.volume = volume;
     }
     private void ShakeRadioByDistance(float distance)
     {
